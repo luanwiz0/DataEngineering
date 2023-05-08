73a091cf00f1bd362d432c5a77ae54f5c6369a27
hyomin Kim <kimhm0828@gmail.com>
2023-05-08 19:18:09 +0900

    add IMDB code

---
+++ HW1/IMDBStudent20200962.java
@@ -0,0 +1,70 @@
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class IMDBStudent20200962 
+{
+
+	public static class IMDBStudentMapper extends Mapper<Object, Text, Text, IntWritable>
+	{
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException 
+		{
+			String genre = value.toString().split("::")[2];
+			StringTokenizer itr = new StringTokenizer(genre.toString(), "|");
+			while(itr.hasMoreTokens())
+			{
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
+
+	public static class IMDBStudentReducer extends Reducer<Text, IntWritable, Text, IntWritable> 
+	{
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException 
+		{
+			int sum = 0;
+			for (IntWritable val : values) 
+			{
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public static void main(String[] args) throws Exception 
+	{
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length != 2) 
+		{
+			System.err.println("Usage: IMDBStudent20200962 <in> <out>");
+			System.exit(2);
+		}
+		Job job = new Job(conf, "IMDB");
+		job.setJarByClass(IMDBStudent20200962.class);
+		job.setMapperClass(IMDBStudentMapper.class);
+		job.setCombinerClass(IMDBStudentReducer.class);
+		job.setReducerClass(IMDBStudentReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		FileSystem.get(job.getConfiguration()).delete( new Path(otherArgs[1]), true);
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
